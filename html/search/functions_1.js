0e497a31546c5b9c2e061c9b31a122a52a1559cd
chrysics <chrysics@gmail.com>
2016-03-16 17:21:05 +0100

    rebuilt documentation

--- html/search/functions_1.js
+++ html/search/functions_1.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['gameover',['gameOver',['../class_gamerunner.html#a3930e1e6659d8933f3bc82911c01d7a5',1,'Gamerunner']]],
+  ['getgame',['getGame',['../class_gamerunner.html#af5156925a0dcf6b9a09298e8745ee956',1,'Gamerunner']]],
   ['getmove',['getMove',['../class_c_l_interface.html#a8f02110f59eeae357387652cf1c8d606',1,'CLInterface']]],
   ['getnextplayermove',['getNextPlayerMove',['../class_gamerunner.html#ac587d78e03e41a7060a8551fb09e52f9',1,'Gamerunner']]],
   ['getpiecesplayer',['getPiecesPlayer',['../class_gamerunner.html#a37e1376ac6b5ba406a3b935f30a1ec61',1,'Gamerunner']]],
