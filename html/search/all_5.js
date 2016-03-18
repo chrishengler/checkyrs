22e480c4b48100fd90124ddfd80ca2a6bfdae06e
chrysics <chrysics@gmail.com>
2016-03-18 14:28:29 +0100

    rebuilt docs

--- html/search/all_5.js
+++ html/search/all_5.js
@@ -5,5 +5,6 @@ var searchData=
   ['interpretsquare',['interpretSquare',['../class_c_l_interface.html#ad0c18baac4a81b982c5869c4808e784d',1,'CLInterface']]],
   ['isdraw',['isDraw',['../class_gamerunner.html#af824b34fd7c1d1d959782cc851456b7b',1,'Gamerunner']]],
   ['isking',['isKing',['../class_square.html#a6c15189bece2f6b0ec5be5168eff30ad',1,'Square']]],
-  ['isoccupied',['isOccupied',['../class_square.html#ac36a72179614062e9b6ffa395f505621',1,'Square']]]
+  ['isoccupied',['isOccupied',['../class_square.html#ac36a72179614062e9b6ffa395f505621',1,'Square']]],
+  ['isstale',['isStale',['../class_game.html#a8e52d0e22be4c644e68acb5e02fec5f3',1,'Game']]]
 ];
