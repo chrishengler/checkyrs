3d03403dc0dfc63be171b58e6b02f3ffe71a42d6
chrysics <chrysics@gmail.com>
2016-03-21 11:37:32 +0100

    rebuilt docs

--- html/search/functions_a.js
+++ html/search/functions_a.js
@@ -3,6 +3,7 @@ var searchData=
   ['save',['save',['../class_checkyrs_a_i.html#a0e952c9d2aa6f1fc91a3c1969baa2b90',1,'CheckyrsAI']]],
   ['serialize',['serialize',['../class_checkyrs_a_i.html#ac04325e693f22eb3e9898238ca524db0',1,'CheckyrsAI']]],
   ['setking',['setKing',['../class_square.html#abe35cdc2d4d854c30fa966dc8e714f1d',1,'Square']]],
+  ['setplayer',['setPlayer',['../class_checkyrs_a_i.html#a1f63ee1d9d1909a8fc333ba85105af88',1,'CheckyrsAI']]],
   ['showgamehelp',['showGameHelp',['../class_c_l_interface.html#ab75d83c54c458dcc8a5e11a5dfa3b85e',1,'CLInterface']]],
   ['showmenuhelp',['showMenuHelp',['../class_c_l_interface.html#abfbeb93c9cda233fc1443df237b7a0f8',1,'CLInterface']]],
   ['showrules',['showRules',['../class_c_l_interface.html#a3a75707e08eee0a80c11dbd091e67ce3',1,'CLInterface']]]
