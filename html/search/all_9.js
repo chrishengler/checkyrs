fcf54c12147ab5fc5aec0e354cc487b15bff11fb
chrysics <chrysics@gmail.com>
2016-03-21 12:35:58 +0100

    rebuilt docs

--- html/search/all_9.js
+++ html/search/all_9.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['m_5fx',['m_x',['../struct_position.html#a8e20797ca5de8fe173cf82e194d09aa6',1,'Position']]],
   ['m_5fy',['m_y',['../struct_position.html#aa8329db9473b0735954340fe7b9f0377',1,'Position']]],
   ['movepiece',['movePiece',['../class_board.html#a14d9da323898779deecb2b7bfccb2e10',1,'Board::movePiece()'],['../class_game.html#a564f68b49a9f74bca1243efb088b7dba',1,'Game::movePiece()']]]
 ];
--- html/search/variables_0.js
+++ html/search/variables_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['m_5fx',['m_x',['../struct_position.html#a8e20797ca5de8fe173cf82e194d09aa6',1,'Position']]],
   ['m_5fy',['m_y',['../struct_position.html#aa8329db9473b0735954340fe7b9f0377',1,'Position']]]
 ];
