34b3e629b0c039e78896266f3b1c60918b414734
chrysics <chrysics@gmail.com>
2016-03-22 19:34:59 +0100

    docs automatically rebuilt

--- html/search/functions_8.js
+++ html/search/functions_8.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['movepiece',['movePiece',['../class_board.html#a14d9da323898779deecb2b7bfccb2e10',1,'Board::movePiece()'],['../class_game.html#a564f68b49a9f74bca1243efb088b7dba',1,'Game::movePiece()']]]
+  ['movepiece',['movePiece',['../class_board.html#a14d9da323898779deecb2b7bfccb2e10',1,'Board']]]
 ];
